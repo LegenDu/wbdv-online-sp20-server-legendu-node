5a581f85b79dcbcf4cbedefc1ff0c4595df8d6be
LegenDu <du.leg@husky.neu.edu>
2020-04-03 14:44:29 -0700

    modified for heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/whiteboard-cs5610-sp20', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/wbdv-online-sp20-server-legendu-node');
+// mongoose.connect('mongodb://localhost:27017/whiteboard-cs5610-sp20', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
